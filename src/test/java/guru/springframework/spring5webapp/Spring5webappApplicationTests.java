9e89a4a590925808e64424c814e6bbf5c76ea113
Saleem B <mdbsaleem@gmail.com>
2021-10-13 08:06:07 +0300

    included publisher repository

--- src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Spring5webappApplicationTests {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
 
 	@Test
 	public void contextLoads() {
